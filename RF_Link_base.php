dbc425d67b074dd65976fa60ea5b9ca6f1fff7e0
Matzel687 <marcel.lanfermann@googlemail.com>
2016-07-28 12:30:43 +0200

    test

--- RF_Link_base.php
+++ RF_Link_base.php
@@ -4,7 +4,7 @@ class RFLinkBase extends IPSModule
 	public function Create()
 	{
 		parent::Create();
-		$this->ConnectParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
+		$this->ConnectParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
 		
 		//{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}
 	}
