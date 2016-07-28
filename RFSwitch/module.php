c0c17d27cfe51bd1390e56be4e2d562051e0e7eb
Matzel687 <marcel.lanfermann@googlemail.com>
2016-07-28 12:28:43 +0200

    test

--- RFSwitch/module.php
+++ RFSwitch/module.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."RF_Link_base.php"); 
 //test
 //test
-class RFSwitch extends RFLinkBase
+class RFLinkSwitch extends RFLinkBase
 {
 	
 	public function Create()
